396e1eebe1db166c863c1677d493a88390a80d4a
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-07-05 13:20:19 -0300

    Create l.cpp

---
+++ NOIC - OBI/l.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define el "\n";
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+
+int recurs(int blocos, int i){
+    
+    if(i == 0){
+       return(0);
+    }
+
+    blocos = (2*i + 1)*(2*i + 1);
+    return(blocos);
+
+}
+
+int main(){
+    _;
+
+    int blocos, aux, i;
+
+    cin >> i;
+
+    for(int j = 0; j < i; j++){
+        aux
+    }
+
+    cout << blocos << el;
+
+
+    return 0;
+}
